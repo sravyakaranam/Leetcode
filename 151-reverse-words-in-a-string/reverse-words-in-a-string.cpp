8b5cb5c01e2194cbff524896c562b5fc629bec8a
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-02-02 14:15:32 -0500

    Time: 10 ms (21.43%) | Memory: 23.1 MB (14.54%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left=0;
+        int right=s.length()-1;
+
+        string temp="";
+        string ans="";
+
+        while(left<=right)
+        {
+            char ch=s[left];
+            if(ch!=' ')
+            {
+                temp+=ch;
+            }
+            else if(ch==' ')
+            {
+                if(temp!=""){
+                if(ans!="")
+                {
+                    ans=temp+" "+ans;
+                }
+                else
+                {
+                    ans=temp;
+                }}
+                temp="";
+            }
+            left++;
+            
+        }
+        if(temp!="")
+        {
+            if(ans!="")
+                {
+                    ans=temp+" "+ans;
+                }
+                else
+                {
+                    ans=temp;
+                }
+        }
+        return ans;
+    }
+};
