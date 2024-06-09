31e7fffb365f0359a8a887f4e5a4046679004f2a
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-06-09 13:07:00 -0400

    Time: 9 ms (35.64%) | Memory: 16.2 MB (11.06%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int cal(int a, int b, const std::string& o)
+    {
+        if (o == "*") {
+            return a * b;
+        } else if (o == "+") {
+            return a + b;
+        } else if (o == "/") {
+            if (b != 0) { // Ensure no division by zero
+                return a / b;
+            } else {
+                throw std::runtime_error("Division by zero.");
+            }
+        } else if (o == "-") {
+            return a - b;
+        } else {
+            throw std::invalid_argument("Unsupported operation");
+        }
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<string> s;
+        for(int i=0;i<tokens.size();i++)
+        {
+            if(!s.empty() && (tokens[i]=="+" ||tokens[i]=="-" ||tokens[i]=="/" ||tokens[i]=="*"))
+            {
+                int b=stoi(s.top());
+                s.pop();
+                int a=stoi(s.top());
+                s.pop();
+                int re=cal(a,b,tokens[i]);
+                s.push(to_string(re));
+
+            }
+            else if(tokens[i]!="+" && tokens[i]!="-" && tokens[i]!="/" && tokens[i]!="*"){
+                s.push(tokens[i]);
+            }
+            
+        }
+        return stoi(s.top());
+    }
+};
