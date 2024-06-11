00bfe71852a10bdd17dcd4badf673c3a41292912
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-06-11 17:40:27 -0400

    Time: 425 ms (5.01%) | Memory: 80.5 MB (86.06%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -7,12 +7,33 @@ public:
         {
             v[i]={position[i],(double)(target-position[i])/speed[i]};
         }
-        //sort(v.begin(),v.end());
-        //reverse(v.begin(),v.end());
+        /*
         sort(v.begin(), v.end());
+        for(int i=0;i<n;i++)
+        {
+            cout<<v[i].first<<" "<<v[i].second<<endl;
+        }
         stack<double> s;
         
         for (int i = n - 1; i >= 0; --i) {
+            // If current car arrives sooner than the last car in the stack
+            // it joins that fleet, so we do not count it as a new fleet.
+            if (s.empty() || v[i].second > s.top()) {
+                s.push(v[i].second);
+            }
+        }
+        */
+        cout<<"**************"<<endl;
+        sort(v.begin(), v.end());
+        reverse(v.begin(), v.end());
+        for(int i=0;i<n;i++)
+        {
+            cout<<v[i].first<<" "<<v[i].second<<endl;
+        }
+
+        stack<double> s;
+        
+        for (int i = 0; i <n; ++i) {
             // If current car arrives sooner than the last car in the stack
             // it joins that fleet, so we do not count it as a new fleet.
             if (s.empty() || v[i].second > s.top()) {
