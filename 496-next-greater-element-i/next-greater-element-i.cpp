39c4cb9680f67a98126683afdc6f33557d6ff84e
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-06-12 17:51:33 -0400

    Time: 0 ms (100.00%) | Memory: 12.2 MB (11.83%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int, int> m;
         vector<int> ans;
 
-        // Iterate through nums2 and fill map with the next greater element for each number
+        
         for (int num : nums2) {
             while (!s.empty() && s.top() < num) {
                 m[s.top()] = num;
@@ -14,13 +14,13 @@ public:
             s.push(num);
         }
         
-        // As there might be elements without a greater next element, map them to -1
+        
         while (!s.empty()) {
             m[s.top()] = -1;
             s.pop();
         }
 
-        // Map the result from nums1 based on computed values for nums2
+        
         for (int num : nums1) {
             ans.push_back(m[num]);
         }
