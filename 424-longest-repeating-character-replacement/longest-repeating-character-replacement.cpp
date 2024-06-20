97fce779f990a964c0a21091ac66cbe926e87c80
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-06-20 11:03:30 -0400

    Time: 15 ms (22.59%) | Memory: 9.6 MB (8.14%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int characterReplacement(std::string s, int k) {
+        int maxCount = 0;  
+        int maxLength = 0;  
+        int l = 0;  
+        std::unordered_map<char, int> charCount;  
+
+        for (int r = 0; r < s.size(); ++r) {
+            charCount[s[r]]++;
+            maxCount = max(maxCount, charCount[s[r]]);
+
+            
+            while (r - l + 1 - maxCount > k) {
+                
+                charCount[s[l]]--;
+                if (charCount[s[l]] == 0) {
+                    charCount.erase(s[l]);
+                }
+                l++;
+            }
+
+            
+            maxLength = max(maxLength, r - l + 1);
+        }
+
+        return maxLength;
+    
+    
+    }
+};
