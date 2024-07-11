7440c87664bb4e87553d9982f49dd6bddc438d70
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2024-07-11 07:57:47 -0400

    Time: 19 ms (10.52%) | Memory: 22.9 MB (17.56%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0; // To count valid placements
+        int count = 0; 
         for (int i = 0; i < flowerbed.size(); i++) {
-            // Check if current spot is empty and both adjacent spots are either empty or non-existent
+           
             if (flowerbed[i] == 0 && 
                 (i == 0 || flowerbed[i - 1] == 0) && 
                 (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
-                flowerbed[i] = 1; // Plant a flower here
-                count++; // Increment count of flowers planted
-                if (count >= n) return true; // Check if we have planted enough flowers
+                flowerbed[i] = 1; 
+                count++; 
+                if (count >= n) return true; 
             }
         }
-        return n <= count; // Check if we were able to plant at least 'n' flowers
-        
+        return n <= count; 
     }
 };
