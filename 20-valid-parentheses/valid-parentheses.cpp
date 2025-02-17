aa5780a6c1f61dd83f58d6c7ce36c7e095b17044
Karanam Sravya <51442803+sravyakaranam@users.noreply.github.com>
2025-02-17 10:22:59 -0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (61.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -6,19 +6,23 @@ public:
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='('||s[i]=='{'||s[i]=='[')st.push(s[i]);
-            else 
+            else
             {
                 if(!st.empty())
                 {
                     char c=st.top();
                     st.pop();
-                    if((c=='('&&s[i]==')')||(c=='{'&&s[i]=='}')||(c=='['&&s[i]==']'))continue;
+                    if((c=='('&&s[i]==')')||(c=='{'&&s[i]=='}')||(c=='[' && s[i]==']'))continue;
                     else return false;
                 }
-                else return false;
+                else
+                {
+                    return false;
+                }
             }
         }
-        if(st.empty())return true;
-        else return false;
+        if(!st.empty())return false;
+        return true;
+        
     }
 };
